ef32742443a75e7ea2a56a1b968fbceee42be091
Stan <Wiekstras@gmail.com>
2022-10-05 13:06:35 +0200

    Update dieletric

--- src/com/wiekstras/Dielectric.java
+++ src/com/wiekstras/Dielectric.java
@@ -10,7 +10,7 @@ public class Dielectric extends Material{
     @Override
     public boolean Scatter(Ray r, HitRecord record, Wrapper wrapper) {
         wrapper.attenuation = new Color(1.0,1.0,1.0);
-        double refractionRatio = record.frontFace ? (1.0/ir) : ir;
+        double refractionRatio = record.frontFace ? (1.0/this.ir) : this.ir;
 
         Vec3 unitDirection = new Vec3().UnitVector(r.GetDirection());
         Vec3 refracted = refract(unitDirection, record.normal, refractionRatio);
@@ -21,15 +21,25 @@ public class Dielectric extends Material{
     }
 
     public Vec3 refract(Vec3 uv, Vec3 n, double etaiOverEtat){
-        double cosTheta = Math.min(Dot(uv.MultiplyByScalar(-1), n), 1.0);
-        // double cosTheta = Double.min(uv.Substract(uv.MultiplyByScalar(2)).Dot(n), 1.0);
 
-        Vec3 rOutPerp = (uv.Add(n.MultiplyByScalar(cosTheta))).MultiplyByScalar(etaiOverEtat);
+        // auto cos_theta = fmin(dot(-uv, n), 1.0);
+        double cosTheta = Math.min(Dot(uv.MultiplyByScalar(-1), n), 1.0);
         
+        // vec3 r_out_perp =  etai_over_etat * (uv + cos_theta*n);
+        Vec3 rOutPerpTemp = uv.Add(n.MultiplyByScalar(cosTheta));
+        Vec3 rOutPerp = rOutPerpTemp.MultiplyByScalar(etaiOverEtat);
         
-        Vec3 temp = n.MultiplyByScalar((-1.0 *  (Math.sqrt(Math.abs(1.0 - rOutPerp.SqrtLength())))));
+        // vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
+        double temp = -Math.sqrt(Math.abs(1.0 - rOutPerp.SqrtLength()));
+        Vec3 rOutParallel = n.MultiplyByScalar(temp);
+        
+
+        // return r_out_perp + r_out_parallel;
+        return rOutPerp.Add(rOutParallel);
+
+
+        // double cosTheta = Double.min(uv.Substract(uv.MultiplyByScalar(2)).Dot(n), 1.0);        
         // Vec3 rOutParallel = n.MultiplyByScalar(temp);
-        return rOutPerp.Add(temp);
     }
   
     public double Dot(Vec3 v, Vec3 n){
